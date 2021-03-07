74037e61010dc64fa9a23acc7853a710b52ca78d
Kohji OKAMURA <okamura-k@ncchd.go.jp>
2021-03-07 10:30:10 +0900

    Julia

---
+++ samples/sum_of_odds/sum_of_odds.jl
@@ -0,0 +1,42 @@
+#!/usr/bin/env julia
+
+#=
+sum_of_odds.jl - calculate sum of odd integers
+=#
+
+__version__ = 1.1
+__date__ = &quot;2021-03-07&quot;
+
+up_to = 170
+
+function is_odd(x::Int64)
+  remainder = x % 2
+  if remainder == 0
+    return false
+  else
+    return true
+  end
+end
+
+sum = number_of_odds = last_odd = 0
+
+for i = 1:up_to
+  if is_odd(i)
+    global sum += i
+    global number_of_odds += 1
+    global last_odd = i
+  end
+end
+
+if sum == number_of_odds ^ 2
+  if number_of_odds == 0
+    ;
+  elseif number_of_odds &lt; 7
+    println(sum)
+  else
+    println(&quot;1 + 3 + 5 + ... + &quot;, last_odd - 1,
+            &quot; + &quot;, last_odd , &quot; = &quot;, sum)
+  end
+else
+  println(stderr, &quot;Unexpected error: &quot;, sum)
+end
